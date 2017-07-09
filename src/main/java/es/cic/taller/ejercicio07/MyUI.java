8b671b898bb03e02a1cd44cc13a5cef4feb60284
pedrogittaller <pedrogittaller@gmail.com>
2017-07-10 00:07:02 +0200

    Cambios menores

--- src/main/java/es/cic/taller/ejercicio07/MyUI.java
+++ src/main/java/es/cic/taller/ejercicio07/MyUI.java
@@ -27,114 +27,104 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
-	
-	       
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-                         
-        final TextField tfNombre = new TextField();
-        tfNombre.setCaption("Nombre:");
 
-        final TextField tfApellidos = new TextField();
-        tfApellidos.setCaption("Apellidos:");
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
 
-        
-        DateTimeField tfFechaNac  = new DateTimeField();
-        tfFechaNac.setCaption("Fecha: ");
-        
-        
-        
-            
-  
-        Collection<Pais> listaPaises = getListaPaises();
-        ComboBox<Pais> cbPais = new ComboBox<>("Select your country", listaPaises);
-        cbPais.setPlaceholder("No country selected");
-        cbPais.setItemCaptionGenerator(Pais::getNombreCompleto);
- 
-        // Disallow null selections
-        cbPais.setEmptySelectionAllowed(false);
- 
-       
-        
-        Button button = new Button("Imprime datos");
-        button.addClickListener( e -> {
-        	Persona persona=new Persona();
-        	persona.setNombre(tfNombre.getValue());
-        	persona.setApellidos(tfApellidos.getValue());
-        	persona.setFechaNac(tfFechaNac.getValue());
-        	
-        	
-        	persona.setFechaNac(tfFechaNac.getValue());
-        	persona.setPais(cbPais.getValue());
-        	
-        	Notification.show(calcularMensaje(persona),
-                       Type.TRAY_NOTIFICATION);        
-	
-        });
-        
-        Button button2 = new Button("Cambia");
-        button2.addClickListener( e -> {
-        	button.setVisible(!button.isVisible());
-//        	if (button.isVisible()) {
-//        		button.setVisible(false);
-//        	} else {
-//        		button.setVisible(true);
-//        	}
-        	
-        });
-        
-        layout.addComponents(tfNombre, tfApellidos, tfFechaNac,  cbPais, button,button2);
-        
-        setContent(layout);
-    }
+		final TextField tfNombre = new TextField();
+		tfNombre.setCaption("Nombre:");
+
+		final TextField tfApellidos = new TextField();
+		tfApellidos.setCaption("Apellidos:");
+
+		DateTimeField tfFechaNac = new DateTimeField();
+		tfFechaNac.setCaption("Fecha: ");
+
+		Collection<Pais> listaPaises = getListaPaises();
+		ComboBox<Pais> cbPais = new ComboBox<>("Selecciona tu país", listaPaises);
+		cbPais.setPlaceholder("Ningún país seleccionado");
+		cbPais.setItemCaptionGenerator(Pais::getNombreCompleto);
+
+		// Disallow null selections
+		cbPais.setEmptySelectionAllowed(false);
+
+		Button button = new Button("Imprime datos");
+		button.addClickListener(e -> {
+			Persona persona = new Persona();
+			persona.setNombre(tfNombre.getValue());
+			persona.setApellidos(tfApellidos.getValue());
+			persona.setFechaNac(tfFechaNac.getValue());
+			persona.setPais(cbPais.getValue());
+
+			Notification.show(calcularMensaje(persona), Type.TRAY_NOTIFICATION);
+
+		});
+
+		Button button2 = new Button("Cambia");
+		button2.addClickListener(e -> {
+			button.setVisible(!button.isVisible());
+			// if (button.isVisible()) {
+			// button.setVisible(false);
+			// } else {
+			// button.setVisible(true);
+			// }
+
+		});
+
+		layout.addComponents(tfNombre, tfApellidos, tfFechaNac, cbPais, button, button2);
+
+		setContent(layout);
+	}
 
 	private String calcularMensaje(Persona persona) {
-		LocalDateTime fecha =persona.getFechaNac();
-		
-		String sFecha="Debe incluir una fecha";
-		if (fecha!=null) {
+		LocalDateTime fecha = persona.getFechaNac();
+
+		String sFecha = "Debe incluir una fecha";
+		if (fecha != null) {
 			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MM yy");
 			sFecha = fecha.format(dtf);
-			}
-		
-		String mensaje=String.format("Mis datos %s %s, estamos a %s en %s", persona.getNombre(), persona.getApellidos(),sFecha, persona.getPais());
+		}
+
+		String mensaje = String.format("Hola %s %s, estamos a %s en %s", persona.getNombre(), persona.getApellidos(),
+				sFecha, persona.getPais());
 		return mensaje;
 	}
 
-	private Collection<Pais> getListaPaises(){
+	private Collection<Pais> getListaPaises() {
 		List<Pais> listaPaises = new ArrayList<>();
-		
-		Pais pais1=new Pais();
+
+		Pais pais1 = new Pais();
 		pais1.setNombreCompleto("España");
-		
+
 		listaPaises.add(pais1);
-		
-		pais1=new Pais();
+
+		pais1 = new Pais();
 		pais1.setNombreCompleto("Alemania");
-		
+
 		listaPaises.add(pais1);
-		
-		pais1=new Pais();
+
+		pais1 = new Pais();
 		pais1.setNombreCompleto("Francia");
-		
+
 		listaPaises.add(pais1);
-		
+
 		return listaPaises;
-		
+
 	}
 
-	
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
 }
